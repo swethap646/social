9576b8a9fcd42fc0c4f040d83732cf5e4eea3746
swethap646 <51953842+swethap646@users.noreply.github.com>
2023-06-09 20:51:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+
+function App() {
+
+  <> 
+  <h1> hiiii</h1>
+  <h2>ejerieorioripoe</h2>
+  <h3>tryuuuy</h3>
+  <h4>fytuiujhu</h4>
+      return  <Profile/>
+      <Home/>
+      <Login/>
+      <Register/>
+      
+      </> 
+}
+
+export default App;
